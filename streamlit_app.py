a8f239972773de550326fe6f9563cfad4c1b3728
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2025-01-29 20:14:36 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🤖💬 Deepseek R1 Chatbot")
+st.set_page_config(page_title="🐳💬 Deepseek R1 Chatbot")
 
 # Replicate Credentials
 with st.sidebar:
